ca58dee3a9f61ab73277d30328f9dbfdc9ab453a
Amey Satwe <amey10051999@gmail.com>
2020-09-14 15:57:52 +0530

    Adding First App for Day-1/Session-1

---
+++ Day1/session_1/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: textDemo(),
+    );
+  }
+}
+
+class textDemo extends StatefulWidget {
+  @override
+  _textDemoState createState() => _textDemoState();
+}
+
+class _textDemoState extends State<textDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Amey Satwe",
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
